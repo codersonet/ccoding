f2714adc998a23389a6c2567935d69f2e1385d0b
codersonet <92801105+codersonet@users.noreply.github.com>
2026-09-06 22:26:20 +0530

    added all q files for labassign2

---
+++ assignments/labassign2/q3.c
@@ -0,0 +1,12 @@
+/*A ‘C’ program contains the following statements.
+#include <stdio.h>
+int a, b;
+float x, y;
+Suppose the value 12 is to be entered into the computer and assigned to a, -8 assigned
+to b, 0.011 assigned to x and -2.2 x 106 assigned to y. Show the input data might most
+conveniently be entered for each of the following scanf functions
+a) scanf (“%d %d %f %f”, &a, &b, &x, &y);
+b) scanf (“%d %d %e %e”, &a, &b, &x, &y);
+c) scanf (“%2d %2d %5f %6e”, &a, &b, &x, &y);
+d) scanf (“%3d %3d %8f %8e”, &a, &b, &x, &y);
+Enter the input and for each input and print the corresponding output.*/
